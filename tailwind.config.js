82bac2e20d536b18996e2429e4ea7546d8861457
Sochuks <moseschukwumakajang@gmail.com>
2022-12-07 17:25:34 +0100

    - Modified tailwind.config   - set values of all screen sizes   - set values of color palette - Modified index   - added header set to relative, marginX-auto   - added flex-box container set to flex, items-center, justify-content.   - added logo - added img to root folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSupLight: 'hsl(12, 88%, 95%)',
+        PaleRed: 'hsl(13, 100%, 96%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayBlue: 'hsl(227, 12%, 61%)',
+        Darkblue: 'hsl(223, 12%, 13%)',
+        LightGray: 'hsl(0, 0%, 98%)',
+      }
+    },
   },
   plugins: [],
 }
