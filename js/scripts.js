46e009bb30737c64897bdf06e5c6020e56f5a3af
Sochuks <moseschukwumakajang@gmail.com>
2022-12-12 11:53:07 +0100

    Modified index added sticky menu
    Added a sticky menu to the website on page scroll

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,9 @@ btn.addEventListener("click", () => {
   nav.classList.toggle('flex')
   nav.classList.toggle('hidden')
 })
+
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function() {
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
